089f8d6aebc32396b648a6ad222f29366e1d4e4d
Veronica-me <54118252+Veronica-me@users.noreply.github.com>
2019-11-18 21:18:39 +0200

    Add files via upload

---
+++ user/show_phones_table.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once '../load.php';
+
+
+$userPhoneTable = new \App\repositories\PhoneDbRepository();
+$phoneArr = $userPhoneTable->getPhoneList();
+
+echo '<table>';
+echo '<tr>';
+echo '<th>phone ID</th>';
+echo '<th>phone number</th>';
+
+echo '</tr>';
+
+foreach ($phoneArr as $value){
+
+    echo '<tr>';
+    echo '<th>' .$value['phone_id'].'</th>';
+    echo '<th>' .$value['phone_number'].'</th>';
+
+    echo '</tr>';
+}
